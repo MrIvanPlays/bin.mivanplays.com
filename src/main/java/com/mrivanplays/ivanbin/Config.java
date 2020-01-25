45615c861b33c2bfe477498795398659d9500a0e
MrIvanPlays <pekov.ivan@abv.bg>
2020-01-25 15:42:13 +0200

    I hate this

--- src/main/java/com/mrivanplays/ivanbin/Config.java
+++ src/main/java/com/mrivanplays/ivanbin/Config.java
@@ -1,12 +1,11 @@
 package com.mrivanplays.ivanbin;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import org.hjson.JsonObject;
 import org.hjson.JsonValue;
 
@@ -15,14 +14,19 @@ public class Config {
   private JsonObject object;
 
   public Config() throws IOException {
-    Path configFile = Paths.get(".", "config.hjson");
-    if (!Files.exists(configFile)) {
-      Path actualFilePath = Files.createFile(configFile);
+    File workdir = new File("");
+    File configFile = new File(workdir, "config.hjson");
+    if (!configFile.exists()) {
+      try {
+        configFile.createNewFile();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
       try (InputStream in = getClass().getClassLoader().getResourceAsStream("config.hjson")) {
-        Files.copy(in, actualFilePath);
+        Files.copy(in, configFile.toPath());
       }
     }
-    try (Reader reader = Files.newBufferedReader(configFile, StandardCharsets.UTF_8)) {
+    try (Reader reader = Files.newBufferedReader(configFile.toPath(), StandardCharsets.UTF_8)) {
       object = JsonValue.readHjson(reader).asObject();
     }
   }
